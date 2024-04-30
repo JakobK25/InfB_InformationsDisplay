04a00c700cb34d484db334da33454facbd53a2bb
JakobK25 <jakobk205@gmail.com>
2024-04-30 10:25:21 +0200

    Scene commit

--- sketch.js
+++ sketch.js
@@ -1,33 +1,74 @@
-let ats_logo
+let ats_logo;
+let tickerText = "TEST - TEST - TEST - TEST - TEST";
+let xPos = 0;
+let speed = 1.7;
 
 function preload() {
-  images = [
-    loadImage('img/ats_logo.png'),
-  ];
+  ats_logo = loadImage("img/ats_logo.png");
+  console.log("preload done");
 }
 
 function setup() {
-  createCanvas(1920, 1080);
-
+  createCanvas(1920, 200);
   mgr = new SceneManager();
   mgr.addScene(dashboard);
   mgr.addScene(booker);
   mgr.addScene(kantine);
-
   mgr.showScene(dashboard);
-
+  frameRate(60); // Adjust frame rate to manage drawing speed
+  console.log("setup done");
 }
 
 function draw() {
+  clear()
   mgr.draw();
+  background(169, 169, 169);
+
+  image(ats_logo, 20, 20, 120, 120);
+  textSize(100)
+  text("AARHUS TECH", 160, 130);
+
+  renderTicker();
+}
+
+function renderTicker() {
+  textSize(50);
+  fill(83, 83, 83);
+
+  rect()
+
+  // Render ticker text
+  text(tickerText, xPos, 190, height);
+
+  // Move ticker text
+  xPos += speed;
+
+  // Check for reset condition
+  if (xPos > width) {
+    xPos = -textWidth(tickerText);
+  }
 }
 
 function dashboard() {
   this.enter = function() {
-    createCanvas(1920, 1080);
-    background(A9A9A9);
+    createCanvas(1920, 200);
+    background(169, 169, 169);
+    console.log("dashboard");
+  }
+}
 
-    image(ats_logo, 0, 0, 200, 200);
-    
+function booker() {
+  this.enter = function() {
+    createCanvas(1920, 1080);
+    background(169, 169, 169);
+    console.log("booker");
+  }
+}
+
+function kantine() {
+  this.enter = function() {
+    createCanvas(1920, 1080);
+    background(169, 169, 169);
+    console.log("kantine");
   }
 }
