e699ca95608ffe9471a7c7c4d9bac6d79b2bdee8
JakobK25 <jakobk205@gmail.com>
2024-05-01 14:18:50 +0200

    Fixed P5js package not loading correct, and added scene changer buttons

--- sketch.js
+++ sketch.js
@@ -1,5 +1,5 @@
 let ats_logo;
-let tickerText = "Husk Fodboldtrøjefredag på fredag! - Brug din stemme: EU-VALGET 9. Juni 2024 - Elever er med i Korners kunstkonkurrence 2024";
+let tickerText = "Husk Fodboldtrøjefredag på fredag! - Brug din stemme. EU-VALGET 9. Juni 2024 - Elever er med i Korners kunstkonkurrence 2024";
 let xPos = 0;
 let speed = 1.7;
 let Skoleplan;
@@ -20,12 +20,12 @@ function setup() {
   frameRate(60); // Adjust frame rate to manage drawing speed
   console.log("setup done");
 
-
 }
 
 function draw() {
   clear()
   mgr.draw();
+  
   background(169, 169, 169);
 
   image(ats_logo, 20, 20, 120, 120);
@@ -63,7 +63,7 @@ function renderTicker() {
 }
 
 function currentTime() {
-  let date = new Date(); 
+  let date = new Date();
   let hh = date.getHours();
   let mm = date.getMinutes();
 
@@ -73,12 +73,35 @@ function currentTime() {
 }
 
 function dashboard() {
+
   this.enter = function () {
-    createCanvas(1920, 200);
+    console.log("hello")
+    createCanvas(1920, 1080);
     background(169, 169, 169);
+
+    let book_btn = createButton('Book Lokale');
+    let debook_btn = createButton('Afbook Lokale');
+    let frokost_btn = createButton('Frokost Menu');
+
+    book_btn.position(275, 1000);
+    debook_btn.position(25, 1000);
+    frokost_btn.position(1640, 1000);
+
+    book_btn.mousePressed(function () {
+      mgr.showScene(booker);
+    });
+
+    debook_btn.mousePressed(function () {
+      mgr.showScene(booker);
+    });
+
+    frokost_btn.mousePressed(function () {
+      mgr.showScene(kantine);
+    });
+    
     console.log("dashboard");
-    }
   }
+}
 
 
 function booker() {
@@ -86,7 +109,7 @@ function booker() {
     console.log("booker");
     createCanvas(1920, 880);
     background(169, 169, 169);
-    
+
     BoxLedig(20, 10);
     BoxLedig(20, 90);
     BoxLedig(20, 170);
@@ -98,7 +121,7 @@ function booker() {
     BoxLedig(20, 650);
     BoxLedig(20, 730);
     BoxLedig(20, 810);
-    
+
     BoxLokale(200, 10, "Lokale SH117")
     BoxLokale(200, 90, "Lokale SH118")
     BoxLokale(200, 170, "Lokale SH208")
@@ -106,41 +129,41 @@ function booker() {
     BoxLokale(200, 330, "Lokale SH213")
     BoxLokale(200, 410, "Lokale SH219")
     BoxLokale(200, 490, "Lokale SH220")
-    
+
   }
-  
+
   function BoxLedig(x, y) {
     // Draw the box
     fill(179, 238, 85);
     rect(x, y, 160, 60, 10);
-    
+
     // Draw the text
     fill(255);
     textSize(35);
     text("Ledig", x + 35, y + 40);
   }
-    
+
   function BoxLokale(x, y, Lokale) {
     // Draw the box
     fill(241, 242, 125);
     rect(x, y, 250, 60, 10);
-    
+
     // Draw the text
     fill(0);
     textSize(35);
     text(Lokale, x + 20, y + 40);
-    
+
   }
-  
-  }
+
+}
 
 
 function kantine() {
-  this.enter = function() {
+  this.enter = function () {
     createCanvas(1920, 880);
     textSize(24);
     textAlign(CENTER, CENTER);
-  
+
     let currentWeekMenus = [
       "Svensk pølseret",
       "Burger",
@@ -148,15 +171,15 @@ function kantine() {
       "Kylling i karry",
       "Pølsemix"
     ];
-  
+
     let nextWeekMenus = [];
-  
+
     let currentWeekNumber;
     let displayCurrentWeek = true;
-  
+
     calculateNextWeek();
     updateMenus();
-  
+
     function calculateNextWeek() {
       nextWeekMenus = [
         "Pita",
@@ -167,62 +190,62 @@ function kantine() {
       ];
       displayCurrentWeek = false;
     }
-  
+
     function updateMenus() {
       let today = new Date();
       currentWeekNumber = getWeekNumber(today);
-  
+
       background(169, 169, 169);
-  
+
       fill(83, 83, 83);
       rect(920, 20, 200, 50, 10);
       fill(255);
       text("Uge " + currentWeekNumber, 1020, 45);
-  
+
       displayWeekMenus(currentWeekMenus, 500, 100);
-  
+
       fill(83, 83, 83);
       rect(920, 420, 200, 50, 10);
       fill(255);
       text("Uge " + (currentWeekNumber + 1), 1020, 445);
-  
+
       displayWeekMenus(nextWeekMenus, 500, 500);
     }
-  
+
     function displayWeekMenus(menus, startX, startY) {
       let spacingX = 225;
       textSize(24);
       fill(255);
-  
+
       let weekdays = ["Mandag", "Tirsdag", "Onsdag", "Torsdag", "Fredag"];
       for (let i = 0; i < weekdays.length; i++) {
         drawWeekdayBox(startX + i * spacingX, startY, weekdays[i]);
         drawMenuBox(startX + i * spacingX, startY + 80, menus[i]);
       }
     }
-  
+
     function drawWeekdayBox(x, y, weekday) {
       let boxWidth = 150;
       let boxHeight = 50;
-  
+
       fill(241, 242, 125);
       rect(x, y, boxWidth, boxHeight, 10);
       fill(0);
       textStyle(BOLD);
       text(weekday, x + boxWidth / 2, y + boxHeight / 2);
     }
-  
+
     function drawMenuBox(x, y, menuText) {
       let boxWidth = 150;
       let boxHeight = 100;
-  
+
       fill(83, 83, 83);
       rect(x, y, boxWidth, boxHeight, 10);
       fill(255);
       textStyle(NORMAL);
       text(menuText, x + boxWidth / 2, y + boxHeight / 2);
     }
-  
+
     function getWeekNumber(date) {
       let onejan = new Date(date.getFullYear(), 0, 1);
       let millisecsInDay = 86400000;
